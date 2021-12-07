13003b22ca640aad3f168d2554d57f0d9b67f6db
Sergey Okishev <so@aspirity.com>
2021-12-07 16:19:41 +0700

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">1</Link>
+            </li>
+            <li>
+              <Link to="/2">2</Link>
+            </li>
+            <li>
+              <Link to="/3">3</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={exmaplePage} />
+          <Route path="/2" element={exmaplePage} />
+          <Route path="/3" element={exmaplePage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const exmaplePage = () => {
+  return <div>Example</div>;
+}
