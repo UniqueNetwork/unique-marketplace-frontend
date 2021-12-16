b13eefe0f9edd20ac95c003a323faef3c06d8ed6
drgosh <drgosh.m@gmail.com>
2021-12-16 15:01:35 +0300

    NFTPAR-947 Fixed extrinsic styles

--- src/pages/Extrinsic/components/ExtrinsicDetail.tsx
+++ src/pages/Extrinsic/components/ExtrinsicDetail.tsx
@@ -26,27 +26,41 @@ const ExtrinsicDetail: FC = () => {
       <h1>Extrinsic {blockIndex}</h1>
       <div className={'grid-container container-with-border'}>
         <div className={'grid-item_col1 text_grey'}>Block </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.block_number}</div>
-        <div className={'grid-item_col1 text_grey'}>Timestamp </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.timestamp && new Date(extrinsic?.view_extrinsic[0]?.timestamp).toLocaleString()}</div>
+        <div className={'grid-item_col11'}>
+          {extrinsic?.view_extrinsic[0]?.block_number}
+        </div>
+        <div className={'grid-item_col1 text_grey margin-top '}>Timestamp </div>
+        <div className={'grid-item_col11 margin-top '}>
+          {extrinsic?.view_extrinsic[0]?.timestamp && new Date(extrinsic?.view_extrinsic[0]?.timestamp).toLocaleString()}
+        </div>
       </div>
       <div className={'grid-container container-with-border margin-top'}>
         <div className={'grid-item_col1 text_grey'}>Sender </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.from_owner && <AccountLinkComponent value={extrinsic?.view_extrinsic[0]?.from_owner} />}</div>
-        <div className={'grid-item_col1 text_grey'}>Destination </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.to_owner && <AccountLinkComponent value={extrinsic?.view_extrinsic[0]?.to_owner} />}</div>
+        <div className={'grid-item_col11'}>
+          {extrinsic?.view_extrinsic[0]?.from_owner && <AccountLinkComponent value={extrinsic?.view_extrinsic[0]?.from_owner} />}
+        </div>
+        <div className={'grid-item_col1 text_grey margin-top '}>Destination </div>
+        <div className={'grid-item_col11 margin-top '}>
+          {extrinsic?.view_extrinsic[0]?.to_owner && <AccountLinkComponent value={extrinsic?.view_extrinsic[0]?.to_owner} />}
+        </div>
       </div>
       <div className={'grid-container container-with-border margin-top'}>
         <div className={'grid-item_col1 text_grey'}>Amount </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.amount}</div>
-        <div className={'grid-item_col1 text_grey'}>Fee </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.fee}</div>
+        <div className={'grid-item_col11'}>
+          {extrinsic?.view_extrinsic[0]?.amount}
+        </div>
+        <div className={'grid-item_col1 text_grey margin-top '}>Fee </div>
+        <div className={'grid-item_col11 margin-top '}>
+          {extrinsic?.view_extrinsic[0]?.fee}
+        </div>
       </div>
       <div className={'grid-container margin-top'}>
         <div className={'grid-item_col1 text_grey'}>Hash </div>
-        <div className={'grid-item_col11'}>{extrinsic?.view_extrinsic[0]?.hash}</div>
-        <div className={'grid-item_col1 text_grey'}>Extrinsic </div>
-        <div className={'grid-item_col11'}>{blockIndex}</div>
+        <div className={'grid-item_col11'}>
+          {extrinsic?.view_extrinsic[0]?.hash}
+        </div>
+        <div className={'grid-item_col1 text_grey margin-top '}>Extrinsic </div>
+        <div className={'grid-item_col11 margin-top '}>{blockIndex}</div>
       </div>
 
     </>
