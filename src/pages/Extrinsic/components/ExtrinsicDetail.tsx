6af9424f0d41aa8098a2e10aedc1c67990c000cd
drgosh <drgosh.m@gmail.com>
2021-12-27 06:43:00 +0300

    Implemented adaptive extrinsic page

--- src/pages/Extrinsic/components/ExtrinsicDetail.tsx
+++ src/pages/Extrinsic/components/ExtrinsicDetail.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
-import {
-  extrinsicQuery,
-  Data as extrinsicData,
-  Variables as ExtrinsicVariables,
-} from '../../../api/graphQL/extrinsic'
+import { Data as extrinsicData, extrinsicQuery, Variables as ExtrinsicVariables } from '../../../api/graphQL/extrinsic'
 import AccountLinkComponent from '../../Account/components/AccountLinkComponent'
 import LoadingComponent from '../../../components/LoadingComponent'
 import { Heading } from '@unique-nft/ui-kit'
+import useDeviceSize, { DeviceSize } from '../../../hooks/useDeviceSize'
+import { shortcutText } from '../../../utils/textUtils'
 
 const ExtrinsicDetail: FC = () => {
   const { blockIndex } = useParams()
@@ -22,6 +20,8 @@ const ExtrinsicDetail: FC = () => {
     fetchPolicy: 'network-only',
     notifyOnNetworkStatusChange: true,
   })
+
+  const deviceSize = useDeviceSize()
 
   if (!blockIndex) return null
 
@@ -37,44 +37,45 @@ const ExtrinsicDetail: FC = () => {
     hash,
   } = extrinsics?.view_extrinsic[0] || {}
 
+
   return (<>
     <Heading>{`Extrinsic ${blockIndex}`}</Heading>
-    <div className={'grid-container container-with-border extrinsic-block-container'}>
+    <div className={'grid-container container-with-border grid-container_extrinsic-container'}>
       <div className={'grid-item_col1 text_grey'}>Block</div>
       <div className={'grid-item_col11'}>{blockNumber}</div>
-      <div className={'grid-item_col1 text_grey margin-top '}>Timestamp</div>
-      <div className={'grid-item_col11 margin-top '}>
+      <div className={'grid-item_col1 text_grey'}>Timestamp</div>
+      <div className={'grid-item_col11'}>
         {timestamp && new Date(timestamp * 1000).toLocaleString()}
       </div>
     </div>
-    <div className={'grid-container container-with-border extrinsic-block-container'}>
+    <div className={'grid-container container-with-border grid-container_extrinsic-container'}>
       <div className={'grid-item_col1 text_grey'}>Sender</div>
       <div className={'grid-item_col11'}>
         {fromOwner && (
-          <AccountLinkComponent value={fromOwner} />
+          <AccountLinkComponent value={fromOwner} noShort={deviceSize !== DeviceSize.sm} />
         )}
       </div>
-      <div className={'grid-item_col1 text_grey margin-top '}>Destination</div>
-      <div className={'grid-item_col11 margin-top '}>
+      <div className={'grid-item_col1 text_grey'}>Destination</div>
+      <div className={'grid-item_col11'}>
         {toOwner && (
-          <AccountLinkComponent value={toOwner} />
+          <AccountLinkComponent value={toOwner} noShort={deviceSize !== DeviceSize.sm} />
         )}
       </div>
     </div>
-    <div className={'grid-container container-with-border extrinsic-block-container'}>
+    <div className={'grid-container container-with-border grid-container_extrinsic-container'}>
       <div className={'grid-item_col1 text_grey'}>Amount</div>
       {/* TODO: due to API issues - amount of some transactions is object which is, for now, should be translated as zero */}
       <div className={'grid-item_col11'}>
         {Number(amount) || 0} QTZ
       </div>
-      <div className={'grid-item_col1 text_grey margin-top '}>Fee</div>
-      <div className={'grid-item_col11 margin-top '}>{Number(fee) || 0} QTZ</div>
+      <div className={'grid-item_col1 text_grey'}>Fee</div>
+      <div className={'grid-item_col11'}>{Number(fee) || 0} QTZ</div>
     </div>
-    <div className={'grid-container extrinsic-block-container'}>
+    <div className={'grid-container grid-container_extrinsic-container'}>
       <div className={'grid-item_col1 text_grey'}>Hash</div>
-      <div className={'grid-item_col11'}>{hash}</div>
-      <div className={'grid-item_col1 text_grey margin-top '}>Extrinsic</div>
-      <div className={'grid-item_col11 margin-top '}>{blockIndex}</div>
+      <div className={'grid-item_col11'}>{hash && deviceSize !== DeviceSize.sm ? hash : shortcutText(hash!)}</div>
+      <div className={'grid-item_col1 text_grey'}>Extrinsic</div>
+      <div className={'grid-item_col11'}>{blockIndex}</div>
     </div>
   </>)
 }
