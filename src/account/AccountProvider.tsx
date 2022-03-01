a80ecee9815764b6b34b10ca07b633d172fa85b3
Sergey Okishev <so@aspirity.com>
2022-03-01 17:34:01 +0700

    Added localAccounts to AllAccounts in accounts context.

--- src/account/AccountProvider.tsx
+++ src/account/AccountProvider.tsx
@@ -1,10 +1,23 @@
 import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import { BN } from '@polkadot/util';
+import keyring from '@polkadot/ui-keyring';
+import { sleep } from '../utils/helpers';
 
 import { AccountProvider } from './AccountContext';
 
 const DefaultAccountKey = 'unique_market_account_address';
+export enum AccountSigner {
+  extension = 'Extension',
+  local = 'Local'
+}
+export interface Account extends InjectedAccountWithMeta {
+  signerType: AccountSigner,
+  balance?: {
+    KSM?: BN
+  }
+}
 
 const AccountWrapper: FC = ({ children }) => {
   const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
@@ -17,6 +30,45 @@ const AccountWrapper: FC = ({ children }) => {
     setSelectedAccount(account);
   }, []);
 
+  const getExtensionAccounts = useCallback(async () => {
+    // this call fires up the authorization popup
+    let extensions = await web3Enable('my cool dapp');
+    if (extensions.length === 0) {
+      console.log('Extension not found, retry in 1s');
+      await sleep(1000);
+      extensions = await web3Enable('my cool dapp');
+      if (extensions.length === 0) {
+        // alert('no extension installed, or the user did not accept the authorization');
+        return [];
+      }
+    }
+    return (await web3Accounts()).map((account) => ({ ...account, signerType: AccountSigner.extension })) as Account[];
+  }, []);
+
+  const getLocalAccounts = useCallback(() => {
+    const keyringAccounts = keyring.getAccounts();
+    return keyringAccounts.map((account) => ({ address: account.address, meta: account.meta, signerType: AccountSigner.local } as Account));
+  }, []);
+
+  const getAccountBalance = useCallback((account: Account) => {
+    return {
+      ...account,
+      balance: {
+        KSM: 0 // rawKusamaRpcApi?.derive.balances?.all(account.address) as number
+      }
+    } as unknown as Account;
+  }, [/* rawKusamaRpcApi */]);
+
+  const getAccounts = useCallback(async () => {
+    // this call fires up the authorization popup
+    const extensionAccounts = await getExtensionAccounts();
+    const localAccounts = getLocalAccounts();
+
+    const allAccounts: Account[] = [...extensionAccounts, ...localAccounts];
+
+    return allAccounts;
+  }, [getExtensionAccounts, getLocalAccounts]);
+
   const fetchAccounts = useCallback(async () => {
     setIsLoading(true);
     // this call fires up the authorization popup
@@ -25,19 +77,20 @@ const AccountWrapper: FC = ({ children }) => {
       setFetchAccountsError('No extension installed, or the user did not accept the authorization');
       return;
     }
-    const allAccounts = await web3Accounts();
+    const allAccounts = await getAccounts();
+    const accountsWithBalance = allAccounts.map((account) => getAccountBalance(account));
 
     if (allAccounts?.length) {
-      setAccounts(allAccounts);
+      setAccounts(accountsWithBalance);
       setIsLoading(false);
 
       const defaultAccountAddress = localStorage.getItem(DefaultAccountKey);
-      const defaultAccount = allAccounts.find((item) => item.address === defaultAccountAddress);
+      const defaultAccount = accountsWithBalance.find((item) => item.address === defaultAccountAddress);
 
       if (defaultAccount) {
         changeAccount(defaultAccount);
       } else {
-        changeAccount(allAccounts[0]);
+        changeAccount(accountsWithBalance[0]);
       }
     } else {
       setFetchAccountsError('No accounts in extension');
