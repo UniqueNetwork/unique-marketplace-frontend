1f61bb885639a1b1e1f2cfa368b30cfd4c553ae1
Sergey Okishev <so@aspirity.com>
2022-03-03 18:05:46 +0700

    Added FAQ page.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 import { MarketPage as Market } from './Market';
 import { MyTokensPage as MyTokens } from './MyTokens';
 import { TradesPage as Trades } from './Trades';
-import { FAQPage as FAQ } from './FAQ';
+import FAQ from './FAQ';
 import TokenPage from './Token'; // temporary page for debugging
 
 export { Market, MyTokens, Trades, FAQ, TokenPage };
