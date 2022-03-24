e4c6022d5ce0a5359fac8a437a0e55f40b1539f5
Anna Mikhailova <amikhailova@usetech.com>
2022-03-24 23:06:25 +0300

    fix: NFTPAR-1389 - correct text in starting auction modal

--- src/hooks/marketplaceStages/useSellFixStages.ts
+++ src/hooks/marketplaceStages/useSellFixStages.ts
@@ -8,25 +8,25 @@ export const useSellFixStages = (collectionId: number, tokenId: number) => {
   const { api } = useApi();
   const marketApi = api?.market;
   const sellFixStages: MarketplaceStage<TFixPriceProps>[] = useMemo(() => [{
-    title: 'Add to WhiteList',
+    title: 'Register sponsorship',
     description: '',
     status: StageStatus.default,
     action: (params) => marketApi?.addToWhiteList(params.txParams.accountAddress, params.options)
   },
   {
-    title: 'Locking NFT for sale',
+    title: 'Prepare NFT to be sent to smart contract',
     description: '',
     status: StageStatus.default,
     action: (params) => marketApi?.lockNftForSale(params.txParams.accountAddress, params.collectionId.toString(), params.tokenId.toString(), params.options)
   },
   {
-    title: 'Sending NFT to Smart contract',
+    title: 'Allow smart contract to receive an NFT',
     description: '',
     status: StageStatus.default,
     action: (params) => marketApi?.sendNftToSmartContract(params.txParams.accountAddress, params.collectionId.toString(), params.tokenId.toString(), params.options)
   },
   {
-    title: 'Setting price',
+    title: 'Set the NFT price',
     description: '',
     status: StageStatus.default,
     action: (params) => marketApi?.setForFixPriceSale(params.txParams.accountAddress, params.collectionId.toString(), params.tokenId.toString(), params?.txParams?.price.toString() || '-1', params.options)
