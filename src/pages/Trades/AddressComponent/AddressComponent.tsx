08815375ac92388f8b01f74ba10ac9094928da43
drgosh <drgosh.m@gmail.com>
2022-03-31 08:59:29 +0300

    Fixed links addresses in table of trades

--- src/pages/Trades/AddressComponent/AddressComponent.tsx
+++ src/pages/Trades/AddressComponent/AddressComponent.tsx
@@ -1,8 +1,19 @@
 import React, { useMemo } from 'react';
 import { shortcutText } from '../../../utils/textUtils';
-import { Text } from '@unique-nft/ui-kit';
+import { Link } from '@unique-nft/ui-kit';
+import config from '../../../config';
+import styled from 'styled-components/macro';
 
 export const AddressComponent = ({ text }: { text: string }) => {
   const shortCut = useMemo(() => (shortcutText(text)), [text]);
-  return <Text>{shortCut}</Text>;
+  return <LinkWrapper><Link
+    href={`${config?.scanUrl || ''}account//${text}`}
+    title={shortCut}
+  /></LinkWrapper>;
 };
+
+const LinkWrapper = styled.span`
+  .unique-link {
+    font-size: 16px;
+  }
+`;
