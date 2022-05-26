f28f27c5e03d45672de2cb8fd7624e0eaeb5b7f1
Ivan Pankrashin <ipankrashin@usetech.com>
2022-05-26 18:03:53 +0700

    move tooltip to a separate component

--- src/pages/Accounts/index.tsx
+++ src/pages/Accounts/index.tsx
@@ -25,7 +25,8 @@ import useDeviceSize, { DeviceSize } from '../../hooks/useDeviceSize';
 import config from '../../config';
 import { TWithdrawBid } from '../../api/restApi/auction/types';
 import { TextInput } from '../../components/TextInput/TextInput';
-import { Primary500, AdditionalDark, AdditionalLight } from '../../styles/colors';
+import { Primary500 } from '../../styles/colors';
+import AccountTooltip from './AccountTooltip';
 
 const tokenSymbol = 'KSM';
 
@@ -146,7 +147,6 @@ export const AccountsPage = () => {
   const [selectedAddress, setSelectedAddress] = useState<string>();
   const deviceSize = useDeviceSize();
   const { chainData } = useApi();
-  const [accountPopupVisible, setAccountPopupVisible] = useState(false);
 
   useEffect(() => {
     if (isLoading) return;
@@ -237,22 +237,6 @@ export const AccountsPage = () => {
     setCurrentModal(undefined);
   }, []);
 
-  useEffect(() => {
-    const questionIcon = document.querySelector('[data-testid="icon-question"]');
-    const onQuestionHover = (e: MouseEvent) => {
-      if (
-        e.target === questionIcon ||
-        (e.target as HTMLElement).closest('[data-testid="icon-question"]') ||
-        (e.target as HTMLElement).closest('[id="account-popup"]')) {
-        setAccountPopupVisible(true);
-      } else setAccountPopupVisible(false);
-    };
-    document.addEventListener('mouseover', onQuestionHover);
-    return () => {
-      document.removeEventListener('mouseover', onQuestionHover);
-    };
-  }, []);
-
   return (<PagePaper>
     <AccountPageWrapper>
       <Row>
@@ -272,12 +256,7 @@ export const AccountsPage = () => {
         </SearchInputWrapper>
       </Row>
       <TableWrapper>
-        {accountPopupVisible && <TooltipStyled
-          id={'account-popup'}
-        >Substrate account addresses (Kusama, Quartz, Polkadot, Unique, etc.) may be represented by a different address
-          character sequence, but they can be converted between each other because they share the same public key. You
-          can see all transformations for any given address on <a href='https://quartz.subscan.io/' target='_blank' rel='noreferrer'>Subscan</a>.
-        </TooltipStyled>}
+        <AccountTooltip/>
         <Table
           columns={getAccountsColumns({ isShortAddress: deviceSize === DeviceSize.sm, formatAddress, onShowSendFundsModal, onShowWithdrawDepositModal })}
           data={filteredAccounts}
@@ -404,37 +383,6 @@ const IconWrapper = styled.div`
     path {
       stroke: currentColor;
     }
-  }
-`;
-
-const TooltipStyled = styled.div`
-  position: absolute;
-  background: ${AdditionalDark};
-  width: 400px;
-  color: ${AdditionalLight};
-  z-index: 1;
-  left: 95px;
-  top: 10px;
-  padding: 8px 16px;
-  line-height: 22px;
-  border-radius: 2px;
-  ::before {
-    position: absolute;
-    content: "";
-    width: 0;
-    height: 0;
-    left: -7px;
-    top: 5px;
-    border-top: 5px solid transparent;
-    border-right: 7px solid ${AdditionalDark};
-    border-bottom: 5px solid transparent;
-  }
-  a {
-    color: ${AdditionalLight};
-    text-decoration: underline;
-  }
-  a:hover {
-    text-decoration: none;
   }
 `;
 
