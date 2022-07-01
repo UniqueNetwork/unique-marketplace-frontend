5b48f8512fb610c74d9aa7ff06a600f68e084020
Ivan Pankrashin <ipankrashin@usetech.com>
2022-07-01 12:17:51 +0700

    hide sell fee for auctions and white listed accounts

--- src/pages/Token/Modals/SellModal.tsx
+++ src/pages/Token/Modals/SellModal.tsx
@@ -148,12 +148,12 @@ export const AskSellModal: FC<TAskSellModalProps> = ({ onSellAuction, onSellFixP
         onChange={onPriceInputChange}
         value={priceInputValue?.toString()}
       />
-      <TextStyled
+      {!selectedAccount?.isOnWhiteList && <TextStyled
         color='additional-warning-500'
         size='s'
       >
         {`A fee of ~ ${kusamaFee} ${tokenSymbol} can be applied to the transaction`}
-      </TextStyled>
+      </TextStyled>}
       <ButtonWrapper>
         <Button
           disabled={!priceInputValue || !Number(priceInputValue)}
@@ -186,12 +186,6 @@ export const AskSellModal: FC<TAskSellModalProps> = ({ onSellAuction, onSellFixP
           value={durationSelectValue?.toString()}
         />
       </Row>
-      <TextStyled
-        color='additional-warning-500'
-        size='s'
-      >
-        {`A fee of ~ ${kusamaFee} ${tokenSymbol} can be applied to the transaction`}
-      </TextStyled>
       <ButtonWrapper>
         <Button
           disabled={!isAuctionValid}
